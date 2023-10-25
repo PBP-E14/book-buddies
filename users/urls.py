84eba04b2340608baced75917c4a048a25b77fcd
kilauakhyaari <kilauakhyaari6@gmail.com>
2023-10-26 00:15:18 +0700

    Progress user profile, register form with ajax

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='user_login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    path('profile/', views.user_profile, name='user_profile'),
+    path('update_profile/', views.update_profile, name='update_profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('user_login')  # Redirect to the login page after successful registration
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'success': True})
+            else:
+                return redirect('user_login')
+        else:
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'success': False, 'errors': form.errors})
     else:
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
 
+def user_profile(request):
+    return render(request, 'profile.html')
+
+@login_required
+def update_profile(request):
+    if request.method == 'POST' and request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        user = request.user
+        user.gender = request.POST.get('gender')
+        user.birth_date = request.POST.get('birth_date')
+        user.phone_number = request.POST.get('phone_number')
+        user.address = request.POST.get('address')
+        user.bio = request.POST.get('bio')
+        user.save()
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False})
