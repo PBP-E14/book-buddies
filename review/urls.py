2cdb36dbfe00ae7e0afee07764ee1f9e3b93816c
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2023-10-29 03:20:02 +0700

    fix card

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from review.views import show_review, create_review
+from review.views import show_review, create_review, get_review_json, add_review_ajax
 
 urlpatterns = [
     path('show_review/', show_review, name='show_review'),
     path('create_review', create_review, name='create_review'),
+    path('get-review/', get_review_json, name='get_review_json'),
+    path('create-review-ajax/', add_review_ajax, name='add_review_ajax')
 ]
