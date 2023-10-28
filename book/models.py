bc4ef2737eacfbf7e69762cddfc1ab96643ddc9d
IrPanTech <irfannkamill@gmail.com>
2023-10-28 10:27:13 +0700

    update model

--- book/models.py
+++ book/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from users.models import User
 
 # Create your models here.
 
@@ -10,8 +9,3 @@ class Book(models.Model):
     publisher = models.CharField(max_length=255)
     year_publication = models.IntegerField()
     image_cover = models.CharField(max_length=2000)
-
-
-class HasReadBook(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
