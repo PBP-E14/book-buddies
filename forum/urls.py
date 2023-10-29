99dcdcf9d8d1e18bd0242e83907e94da08eed2d1
wirr <prawirarzkn94@gmail.com>
2023-10-29 21:14:32 +0700

    Fix delete for admin

--- forum/urls.py
+++ forum/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from forum.views import show_forums, read_forum, back_to_homepage, get_forum_json, add_forum_ajax, \
+from forum.views import show_forums, read_forum, get_forum_json, add_forum_ajax, \
     get_reply_json, add_reply_ajax, get_user_json, remove_forum_button, remove_reply_button
 
 urlpatterns = [
     path('show_forums/', show_forums, name='show_forums'),
     path('show_forums/read_forum/<int:id>/', read_forum, name='read_forum'),
-    path('back_to_homepage/', back_to_homepage, name='back_to_homepage'),
     path('get_forum_json/<int:choice>/', get_forum_json, name='get_forum_json'),
     path('add_forum_ajax/', add_forum_ajax, name='add_forum_ajax'),
     path('get_reply_json/<int:id>/', get_reply_json, name='get_reply_json'),
