616f9f90335497b0ff90a58e62166b7b63b9313b
wirr <prawirarzkn94@gmail.com>
2023-12-16 01:06:00 +0700

    Update untuk keperluan forum di flutter

--- forum/urls.py
+++ forum/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from forum.views import show_forums, read_forum, get_forum_json, add_forum_ajax, \
-    get_reply_json, add_reply_ajax, get_user_json, remove_forum_button, remove_reply_button
+    get_reply_json, add_reply_ajax, get_user_json, remove_forum_button, remove_reply_button, show_json_forum, \
+    show_json_reply, show_json_reply_byId, create_forum_flutter
 
 urlpatterns = [
     path('show_forums/', show_forums, name='show_forums'),
@@ -12,4 +13,8 @@ urlpatterns = [
     path('get_user_json/', get_user_json, name='get_user_json'),
     path('remove_forum_button/<int:id>/', remove_forum_button, name='remove_forum_button'),
     path('remove_reply_button/<int:id>/', remove_reply_button, name='remove_reply_button'),
+    path('show_json_forum/', show_json_forum, name='show_json_forum'),
+    path('show_json_reply/', show_json_reply, name='show_json_reply'),
+    path('show_json_reply_byId/<int:id>/', show_json_reply_byId, name='show_json_reply_byId'),
+    path('create-forum-flutter/', create_forum_flutter, name='create_forum_flutter'),
 ]
